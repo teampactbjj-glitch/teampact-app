7846e66fa15cd9eb5007fe1658cff6a47d54dbc7
dudi ben zaken <dudibenzaken@h-MacBook-Pro-sl-dudi.local>
2026-04-11 18:56:41 +0300

    fix: update Supabase anon key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = 'https://pnicoluujpidguvniwub.supabase.co'
-const SUPABASE_KEY = 'sb_publishable_dlgsMcrgBrxR8G0w9hiqhw_RXAbxYmY'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBuaWNvbHV1anBpZGd1dm5pd3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTY2NjUsImV4cCI6MjA5MTI5MjY2NX0.I7bRbvy1eU-W3MrlHuB93v2nGffsA9oiapfaa3SX6nM'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
